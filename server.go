63a28a9a51b77efcba94c5c3968bbd28ced0db92
melsincostan <139009476+melsincostan@users.noreply.github.com>
2024-09-10 12:19:26 +0200

    add tls support (bring your own cert)

--- server.go
+++ server.go
@@ -34,8 +34,17 @@ func main() {
 		fmt.Print("\n###---###\n\n")
 		w.WriteHeader(204)
 	})
-	fmt.Printf("Beep! :>\nListening on port %s\n", os.Args[1])
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", os.Args[1]), nil); err != nil {
-   fmt.Printf("Boop! :<\nGot an error trying to listen on port %s: \"%s\"\n", os.Args[1], err.Error())
- }
+	if len(os.Args) == 5 && os.Args[2] == "tls" {
+		fmt.Printf("Beep! :>\nListening on port %s (with tls: cert %s / key %s)\n", os.Args[1], os.Args[3], os.Args[4])
+		if err := http.ListenAndServeTLS(fmt.Sprintf(":%s", os.Args[1]), os.Args[3], os.Args[4], nil); err != nil {
+			fmt.Printf("Boop! :<\nGot an error trying to listen on port %s (with tls: cert %s / key %s): \"%s\"\n", os.Args[1], os.Args[3], os.Args[4], err.Error())
+			os.Exit(1)
+		}
+	} else {
+		fmt.Printf("Beep! :>\nListening on port %s\n", os.Args[1])
+		if err := http.ListenAndServe(fmt.Sprintf(":%s", os.Args[1]), nil); err != nil {
+			fmt.Printf("Boop! :<\nGot an error trying to listen on port %s: \"%s\"\n", os.Args[1], err.Error())
+			os.Exit(1)
+		}
+	}
 }
